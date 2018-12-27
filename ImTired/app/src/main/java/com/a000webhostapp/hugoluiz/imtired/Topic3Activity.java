c233be18ac5c82c979fe72d73f94683c005a9a80
hugo lazzari <hugo.luiz@outlook.com>
2018-12-27 02:52:41 -0200

    added new activities

---
+++ ImTired/app/src/main/java/com/a000webhostapp/hugoluiz/imtired/Topic3Activity.java
@@ -0,0 +1,13 @@
+package com.a000webhostapp.hugoluiz.imtired;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Topic3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic3);
+    }
+}
